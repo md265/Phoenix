b5bf64e4200e887b649215436daf34498ca88ea7
Maciej Domosud <md1@wp.pl>
2023-09-12 11:23:27 +0200

    main fucntion

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,21 +15,60 @@
 /* Data structure init */
 /// @brief An array of the tree roots for each mask (CIDR). Index is shifted by
 /// 1 - i.e. table index 15 corresponds to mask "/16".
-static node_t* mask_table[MAX_MASK] = {NULL};
+static node_t* prefix_table[MAX_MASK] = {NULL};
 
 #include "add.c"
 #include "check.c"
 #include "del.c"
 
 int main(void) {
-  // prefix_t test_prefix = {.ip = 0xAABBCCDD, .mask = 10};
+  int result;
 
-  // unsigned char bytes[4];
-  // bytes[0] = test_prefix.ip & 0xFF;
-  // bytes[1] = (test_prefix.ip >> 8) & 0xFF;
-  // bytes[2] = (test_prefix.ip >> 16) & 0xFF;
-  // bytes[3] = (test_prefix.ip >> 24) & 0xFF;
-  // printf("%d.%d.%d.%d\n", bytes[3], bytes[2], bytes[1], bytes[0]);
+  result = add(0X0A000000, 16);
+  printf("Adding 10.0.0.0/16 : result = %d\n", result);
+  assert(0 == result);
+
+  result = add(0x0A140000, 16);
+  printf("Adding 10.20.0.0/16 : result = %d\n", result);
+  assert(0 == result);
+
+  result = add(0x0A000000, 8);
+  printf("Adding 10.0.0.0/8 : result = %d\n", result);
+  assert(0 == result);
+
+  printf("\n");
+
+  result = check(0x0A14FFFF);
+  printf("Checking 10.20.255.255 : result = %d\n", result);
+  assert(16 == result);
+
+  result = check(0x0A150000);
+  printf("Checking 10.21.0.0 : result = %d\n", result);
+  assert(-1 == result);
+
+  printf("\n");
+
+  result = add(0X0A000000, 16);
+  printf("Adding 10.0.0.0/16 again: result = %d\n", result);
+  assert(-1 == result);
+
+  printf("\n");
+
+  result = add(0X020408000, 16);
+  printf("Adding 32.64.128.0/16 : result = %d\n", result);
+  assert(0 == result);
+
+  printf("\n");
+
+  result = del(0X0A000000, 16);
+  printf("Deleting 10.0.0.0/16 : result = %d\n", result);
+  assert(0 == result);
+
+  result = del(0x0A000000, 8);
+  printf("Deleting 10.0.0.0/8 : result = %d\n", result);
+  assert(0 == result);
+
+  printf("\n");
 
   return 0;
 }
